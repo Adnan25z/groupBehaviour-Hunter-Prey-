2751bed9ce22cf293e0e6abcc4aaa58e6ffc2b31
Adnan <100885766+Adnan25z@users.noreply.github.com>
2023-11-07 22:51:15 +1100

    Add files via upload

---
+++ emergent/agent.py
@@ -0,0 +1,255 @@
+'''An agent with Seek, Flee, Arrive, Pursuit behaviours
+Created for COS30002 AI for Games by Clinton Woodward <cwoodward@swin.edu.au>
+For class use only. Do not publically share or post this code without permission.
+'''
+from vector2d import Vector2D
+from vector2d import Point2D
+from graphics import egi, KEY
+from math import sin, cos, radians
+from random import random, randrange, uniform
+
+class Agent(object):
+    def __init__(self, world=None, scale=30.0, mass=1.0, mode='seek', is_predator=False):
+        # keep a reference to the world object
+        self.world = world
+        self.mode = mode
+        self.is_predator = is_predator
+        # Physics for self
+        dir = radians(random()*360)
+        self.pos = Vector2D(randrange(world.cx), randrange(world.cy))
+        self.vel = Vector2D()
+        self.heading = Vector2D(sin(dir), cos(dir))
+        self.force = Vector2D()
+        self.side = self.heading.perp()
+        self.scale = Vector2D(scale, scale)  # easy scaling of agent size
+        self.force = Vector2D()  # current steering force
+        self.accel = Vector2D() # current acceleration due to force
+        self.mass = mass
+
+        # Appearance variables
+        self.color = 'ORANGE'
+        self.vehicle_shape = [
+            Point2D(-1.0,  0.6),
+            Point2D( 1.0,  0.0),
+            Point2D(-1.0, -0.6)
+        ]
+
+        # Force and speed limiting variables
+        self.max_speed = 10.0 * scale
+        self.max_force = 600.0
+
+        # Cohesion, separation and alignment steering behaviours
+        self.cohesion = 200.0
+        self.separation = 80.0
+        self.alignment = 400.0
+        self.neighbours = []
+        self.closeby = []
+
+        # Wander Variables
+        self.wander_target = Vector2D(1, 0)
+        self.wander_dist = 3.0 * scale
+        self.wander_radius = 1.0 * scale
+        self.wander_jitter = 0.5 * scale
+        self.bRadius = scale
+
+        # debug draw info?
+        self.show_info = False
+
+    def calculate(self, delta):
+        '''calculate the current steering force'''
+        # If the agent is a predator, use the wander behavior
+        if self.is_predator:
+            return self.wander(delta)
+        # If the agent is not a predator, consider the predator's presence
+        predator = self.get_predator()
+        if predator:
+            distance_to_predator = (predator.pos - self.pos).length()
+            flee_radius = 200  # Adjust this value as needed
+            if distance_to_predator < flee_radius:
+                return self.flee(predator.pos)
+        force = self.wander(delta)
+        if (self.neighbours):
+            force += self.emerge()
+        if (self.closeby):
+            force += self.seperate()
+        if (self.neighbours and not self.closeby):
+            force += self.approach_centre()
+        return force
+
+    def get_predator(self):
+        for agent in self.world.agents:
+            if agent.is_predator:
+                return agent
+        return None
+
+    def update(self, delta):
+        ''' update vehicle position and orientation '''
+        # calculate and set self.force to be applied
+        force = self.calculate(delta)
+        self.force += force
+        # determine the new acceleration
+        self.accel = self.force / self.mass  # not needed if mass = 1.0
+        # new velocity
+        self.vel += self.accel * delta
+        # check for limits of new velocity
+        self.vel.truncate(self.max_speed)
+        # update position
+        self.pos += self.vel * delta
+        # update heading is non-zero velocity (moving)
+        if self.vel.lengthSq() > 0.00000001:
+            self.heading = self.vel.get_normalised()
+            self.side = self.heading.perp()
+        # treat world as continuous space - wrap new position if needed
+        self.world.wrap_around(self.pos)
+
+        # update emerge and separate lists
+        self.neighbours = self.get_agents_in_radius(self.cohesion)
+        self.closeby = self.get_agents_in_radius(self.separation)
+
+        # limit force? <-- for wander
+        self.force.truncate(self.max_force)
+
+    def render(self, color=None):
+        ''' Draw the triangle agent with color'''
+        # draw the ship
+        egi.set_pen_color(name=self.color)
+        pts = self.world.transform_points(self.vehicle_shape, self.pos,self.heading, self.side, self.scale)
+        # draw it!
+        egi.closed_shape(pts)
+        # add some handy debug drawing info lines - force and velocity
+        if self.show_info:
+            s = 0.5 # <-- scaling factor
+            ### PHYSICS
+            # force
+            egi.red_pen()
+            egi.line_with_arrow(self.pos, self.pos + self.force * s, 5)
+            # net (desired) change
+            egi.grey_pen()
+            egi.line_with_arrow(self.pos, self.pos+ (self.force+self.vel) * s, 5)
+
+            ### EMERGE
+            # cohesion circle
+            egi.pink_pen()
+            egi.circle(self.pos,self.cohesion)
+            # separation circle
+            egi.grey_pen()
+            egi.circle(self.pos,self.separation)
+            if (self.neighbours):
+                # Average Heading Line
+                egi.green_pen()
+                ave_heading = self.neighbour_average('heading')
+                egi.line_with_arrow(self.pos, self.pos + self.alignment * ave_heading * s,5)
+                # Average Position Line
+                egi.blue_pen()
+                ave_position = self.neighbour_average('pos')
+                egi.line_with_arrow(self.pos, ave_position,5)
+            if (self.closeby):
+                egi.aqua_pen()
+                closest_agent_pos = self.closest(self.closeby).pos
+                target = 2 * self.pos - closest_agent_pos
+                egi.line_with_arrow(self.pos, target,5)
+            # Become pink if in neighbourhood of agents[0]
+            for agent in self.world.agents:
+                if agent in self.neighbours:
+                    agent.color = 'PINK'
+                else:
+                    agent.color = 'ORANGE'
+
+            ### WANDER
+            # calculate the center of the wander circle in front of the agent
+            wnd_pos = Vector2D(self.wander_dist, 0)
+            wld_pos = self.world.transform_point(wnd_pos, self.pos, self.heading, self.side)
+            # draw the wander circle
+            egi.green_pen()
+            egi.circle(wld_pos, self.wander_radius)
+            # draw the wander target (little circle on the big circle)
+            egi.red_pen()
+            wnd_pos = (self.wander_target + Vector2D(self.wander_dist, 0))
+            wld_pos = self.world.transform_point(wnd_pos, self.pos, self.heading, self.side)
+            egi.circle(wld_pos, 3)
+
+    def speed(self):
+        return self.vel.length()
+
+    def get_agents_in_radius(self,radius):
+        # Returns a list of all the agents within a given radius of self
+        neighbours = []
+        for agent in self.world.agents:
+            if (agent != self):
+                agent_to_self = self.pos - agent.pos
+                dist = agent_to_self.length()
+                if (dist < radius):
+                    neighbours.append(agent)
+        return neighbours
+    
+    def neighbour_average(self,attr):
+        # Gets the average of a given attribute from all of the neighbour
+        average = Vector2D(0,0)
+        for agent in self.neighbours:
+            average += getattr(agent,attr)
+        average /= len(self.neighbours)
+        return average
+
+    def closest(self,list):
+        # Returns the closest object to self from a given list of objects
+        dist_to_closest = None
+        closest = None
+        # Gets the closest object to hide behind
+        for obj in list:
+            to_obj = obj.pos - self.pos
+            dist = to_obj.length()
+            if (closest is None or dist < dist_to_closest):
+                closest = obj
+                dist_to_closest = dist
+        return closest
+
+    def seek(self, target_pos):
+        ''' move towards target position '''
+        desired_vel = (target_pos - self.pos).normalise() * self.max_speed
+        return (desired_vel - self.vel)
+    
+    def flee(self, target_pos):
+        ''' move away from target position '''
+        desired_vel = (self.pos - target_pos).normalise() * self.max_speed
+        return (desired_vel - self.vel)
+
+    def emerge(self):
+        # Approaches the average heading
+        ave_heading = self.neighbour_average('heading')
+        ave_heading.normalise()
+        target = self.pos + self.alignment * ave_heading
+        to_target = target - self.pos
+        return to_target
+
+    def approach_centre(self):
+        # Approaches the centre
+        ave_position = self.neighbour_average('pos')
+        to_ave = ave_position - self.pos
+        return to_ave
+
+    def seperate(self):
+        # Moves away from nearby agent
+        closest_agent_pos = self.closest(self.closeby).pos
+        target = (2 * self.pos - closest_agent_pos)
+        to_target = target - self.pos
+        return to_target
+
+    def wander(self, delta):
+        ''' random wandering using a projected jitter circle '''
+        wt = self.wander_target
+        # this behaviour is dependent on the update rate, so this line must
+        # be included when using time independent framerate.
+        jitter_tts = self.wander_jitter * delta # this time slice
+        # first, add a small random vector to the target's position
+        wt += Vector2D(uniform(-1,1) * jitter_tts, uniform(-1,1) * jitter_tts)
+        # re-project this new vector back on to a unit circle
+        wt.normalise()
+        # increase the length of the vector to the same as the radius
+        # of the wander circle
+        wt *= self.wander_radius
+        # move the target into a position WanderDist in front of the agent
+        target = wt + Vector2D(self.wander_dist, 0)
+        # project the target into world space
+        wld_target = self.world.transform_point(target, self.pos, self.heading, self.side)
+        # and steer towards it
+        return self.seek(wld_target)
---
+++ emergent/main.py
@@ -0,0 +1,90 @@
+'''Autonomous Agent Movement: Paths and Wandering
+
+Created for COS30002 AI for Games by Clinton Woodward <cwoodward@swin.edu.au>
+
+For class use only. Do not publically share or post this code without permission.
+
+This code is essentially the same as the base for the previous steering lab
+but with additional code to support this lab.
+
+'''
+from graphics import egi, KEY
+from pyglet import window, clock
+from pyglet.gl import *
+
+from vector2d import Vector2D
+from world import World
+from agent import Agent  # Agent with seek, arrive, flee and pursuit
+
+
+SCREEN_WIDTH = 250
+SCREEN_HEIGHT = 250
+
+MODIFIERS = {
+KEY.C: 'cohesion',
+KEY.S: 'separation',
+KEY.A: 'alignment'
+}
+
+CURRENT_MODIFIER = 'cohesion'
+
+def on_key_press(symbol, modifiers):
+    global CURRENT_MODIFIER
+    if symbol == KEY.P:
+        world.paused = not world.paused
+    elif symbol == KEY.O:
+        world.agents.append(Agent(world))
+    elif symbol == KEY.I:
+        world.agents[0].show_info = not world.agents[0].show_info
+    elif symbol in MODIFIERS:
+        CURRENT_MODIFIER = MODIFIERS[symbol]
+    elif symbol == KEY.NUM_ADD:
+        for agent in world.agents:
+            attr = getattr(agent,CURRENT_MODIFIER)
+            setattr(agent, CURRENT_MODIFIER, attr + 20.0)
+    elif symbol == KEY.NUM_SUBTRACT:
+        for agent in world.agents:
+            attr = getattr(agent,CURRENT_MODIFIER)
+            setattr(agent, CURRENT_MODIFIER, attr - 20.0)
+
+def on_resize(cx, cy):
+    world.cx = cx
+    world.cy = cy
+
+
+if __name__ == '__main__':
+
+    # create a pyglet window and set glOptions
+    win = window.Window(width= SCREEN_WIDTH, height=SCREEN_HEIGHT, vsync=True, resizable=True)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    # needed so that egi knows where to draw
+    egi.InitWithPyglet(win)
+    # prep the fps display
+    fps_display = window.FPSDisplay(win)
+    # register key and mouse event handlers
+    win.push_handlers(on_key_press)
+    win.push_handlers(on_resize)
+
+    # create a world for agents
+    world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+    # add x agents
+    for x in range(15):
+        world.agents.append(Agent(world))
+    # Add a predator agent
+    predator = Agent(world, scale=60.0, is_predator=True)
+    world.agents.append(predator)
+    # unpause the world ready for movement
+    world.paused = False
+
+    while not win.has_exit:
+        win.dispatch_events()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # show nice FPS bottom right (default)
+        delta = clock.tick()
+        world.update(delta)
+        world.render()
+        fps_display.draw()
+        # swap the double buffer
+        win.flip()
+
